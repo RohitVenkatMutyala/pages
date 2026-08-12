f8deaef7e91a1ab74a650261cbb2eb3a2a88cb16
RohitVenkatMutyala <rohithvenkatmutyala@gmail.com>
2026-08-13 00:28:37 +0530

    0028

---
+++ api/likes.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  const WORKSPACE = "rohith-venkat-mutyalas-team-5054";
+  try {
+    const r = await fetch(`https://api.counterapi.dev/v2/${WORKSPACE}/likes`, {
+      headers: { Authorization: `Bearer ${process.env.CA_KEY}` }
+    });
+    const data = await r.json();
+    res.status(r.status).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
